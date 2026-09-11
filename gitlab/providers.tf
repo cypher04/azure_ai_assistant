cd45873b76232e2ef1c090210b3c645631525200
Seyi Abiodun <cypher04>
2026-09-11 14:50:30 +0100

    initial commit: create folders and files for project

---
+++ gitlab/providers.tf
@@ -0,0 +1,33 @@
+
+terraform {
+  required_providers {
+
+    azurerm = {
+        source  = "hashicorp/azurerm"
+        version = "~> 4.80.0"
+        }
+
+    azuread = {
+        source  = "hashicorp/azuread"
+        version = "~> 3.9.0"
+        }
+
+        gitlab = {
+        source  = "gitlabhq/gitlab"
+        version = "~> 19.3.0"
+        }
+  }
+}
+
+
+provider "azurerm" {
+  features {}
+}
+
+provider "azuread" {
+  tenant_id = var.tenant_id
+}
+
+provider "gitlab" {
+  token = var.gitlab_token
+}
